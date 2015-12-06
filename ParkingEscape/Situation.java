c797b9db13345557b98d4be566454815f8e67eb3
RobinPetit <robpetit@ulb.ac.be>
2015-12-06 14:21:01 +0100

    implementation of getPossibleMovements

--- ParkingEscape/Situation.java
+++ ParkingEscape/Situation.java
@@ -10,14 +10,20 @@ public class Situation {
 		Up, Down, Left, Right
 	}
 
+	private enum Orientation {
+		Vertical, Horizontal
+	}
+
 	private final Point size;
 	private int[][] parking;
 	List<Point> carsPositions;
+	List<Orientation> carsOrientations;
 
     public Situation(Point size) {
 		this.carsPositions = new Vector<>();
+		this.carsOrientations = new Vector<>();
 		this.size = size;
-		parking = new int[size.x][];
+		parking = new int[size.y][];
 		for(int i = 0; i < size.y; ++i)
 			parking[i] = new int[size.x];
     }
@@ -27,10 +33,12 @@ public class Situation {
 			return;
 		//Verify that there is not another car at specified positions
 		//and that positions are adjacent.
+		if(positions.size() == 1)
+			throw new IllegalArgumentException("Cars must be at least 2 units long.");
 		for(int i = 0; i < positions.size(); ++i) {
 			Point pos = positions.get(i);
 			if(getCar(pos) >= 0) {
-				throw new IllegalArgumentException("There already is a car at specified position.");
+				throw new IllegalArgumentException("There is already a car at specified position.");
 			}
 			if(i > 0) {
 				Point difference = positions.get(i - 1);
@@ -43,21 +51,72 @@ public class Situation {
 		//All is fine, we can add the car
 		int newCar = carsPositions.size();
 		carsPositions.add(positions.get(0));
+		carsOrientations.add(positions.get(0).x - positions.get(1).x == 0 ? Orientation.Horizontal : Orientation.Vertical);
 		for(Point pos : positions)
 			parking[pos.y][pos.x] = newCar;
 	}
-
+	
+	public List<Point> getCarPosition(int car) throws IndexOutOfBoundsException {
+		if(car >= carsPositions.size())
+			throw new IndexOutOfBoundsException("The specified car does not exist");
+		Point pos = carsPositions.get(car);
+		Orientation orientation = carsOrientations.get(car);
+		Vector<Point> ret = new Vector<>();
+		if(orientation == Orientation.Horizontal) {
+			int y = pos.y;
+			while(getCar(pos.x, pos.y) == car) {
+				ret.add(new Point(pos.x, y));
+				++y;
+			}
+			y = pos.y - 1;
+			while(getCar(pos.x, y) == car) {
+				ret.add(0, new Point(pos.x, y));
+				--y;
+			}
+		} else {
+			int x = pos.x;
+			while(getCar(x, pos.y) == car) {
+				ret.add(new Point(x, pos.y));
+				++x;
+			}
+			x = pos.x-1;
+			while(getCar(x, pos.y) == car) {
+				ret.add(0, new Point(x, pos.y)) ;
+				--x;
+			}
+		}
+		return ret;
+	}
 
 	public List<Movement> getPossibleMovements(int car) throws IndexOutOfBoundsException{
 		if(car >= carsPositions.size())
-			throw new IndexOutOfBoundsException("The specified car does not exists.");
+			throw new IndexOutOfBoundsException("The specified car does not exist.");
 		Vector<Movement> result = new Vector<>();
+		List<Point> pos = getCarPosition(car);
+		Orientation orientation = carsOrientations.get(car);
+		Point previousCell = pos.get(0);
+		Point nextCell = pos.get(pos.size()-1);
+		if(orientation == Orientation.Horizontal) {
+			previousCell.translate(-1, 0);
+			if(previousCell.x >= 0 && getCar(previousCell) == 0)
+				result.add(Movement.Left);
+			nextCell.translate(+1, 0);
+			if(nextCell.x < this.size.x && getCar(nextCell) == 0)
+				result.add(Movement.Down);
+		} else {
+			previousCell.translate(0, -1);
+			if(previousCell.y >= 0 && getCar(previousCell) == 0)
+				result.add(Movement.Up);
+			nextCell.translate(0, +1);
+			if(nextCell.y < this.size.y && getCar(nextCell) == 0)
+				result.add(Movement.Down);
+		}
 		return result;
 	}
 
 	public void moveCar(int car, Movement movement) throws IndexOutOfBoundsException, IllegalArgumentException {
 		if(car >= carsPositions.size())
-			throw new IndexOutOfBoundsException("The specified car does not exists.");
+			throw new IndexOutOfBoundsException("The specified car does not exist.");
 		if(!getPossibleMovements(car).contains(movement))
 			throw new IllegalArgumentException("Movement not supported by specified car.");
 		else {
