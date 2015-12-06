49bf19e3c25f19a427abfd3a966a9a5f06072c79
RobinPetit <robpetit@ulb.ac.be>
2015-12-06 16:27:18 +0100

    add tests

--- ParkingEscape/Projet1.java
+++ ParkingEscape/Projet1.java
@@ -1,14 +1,31 @@
 package ParkingEscape;
 
+import java.awt.Point;
+import java.util.Vector;
+import java.util.List;
+
 public class Projet1
 {
-    public static void main(String[] args)
-    {
-        if(args.length != 1) {
-            System.out.println("usage : java Projet1 fichier");
+	public static void main(String[] args)
+	{
+		if(args.length != 1) {
+			System.out.println("usage : java Projet1 fichier");
 		}
-        else {
-            Graphe graphe = GestionnaireES.creerGraphe(args[0]);
+		else {
+			test();
 		}
-    }
+	}
+
+	private static void test() {
+		Situation s = new Situation(new Point(5, 5));
+		Vector<Point> car = new Vector<>();
+		car.add(new Point(1, 3));
+		car.add(new Point(2, 3));
+		car.add(new Point(3, 3));
+		s.addCar(car);
+		System.out.println("Car added");
+		List<Situation.Movement> mvs = s.getPossibleMovements(s.getCar(car.get(0)));
+		for(Situation.Movement mv : mvs)
+			System.out.println(mv.name());
+	}
 }
