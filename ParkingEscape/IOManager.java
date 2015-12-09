fa08815479c52a2a1bee26f8cb04346e6d052362
TheoVerhelst <tverhels@ulb.ac.be>
2015-12-09 15:01:35 +0100

    Adds IOManager.parseListPoint

--- ParkingEscape/IOManager.java
+++ ParkingEscape/IOManager.java
@@ -3,6 +3,9 @@ package ParkingEscape;
 import java.io.*;
 import java.util.List;
 import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.awt.Point;
 import java.text.ParseException;
 
 public class IOManager {
@@ -31,6 +34,19 @@ public class IOManager {
 		}
 		return ret;
     }
+
+	public private List<Point> parseListPoint(String listAsString) throws ParseException{
+		List<Point> ret = new Vector<>();
+		//Get only the bracket-list after the semicolon
+		listAsString = listAsString.split(":")[1].trim();
+		Pattern tuplesPattern = Pattern.compile("\\[(?:\\((\\d+),\\s*(\\d+)\\),\\s*)\\]");
+		Matcher matches = tuplesPattern.matcher(listAsString);
+		if(!matches.matches() || matches.groupCount() % 2 != 0)
+			throw new ParseException("List of positions is badly written.", 0);
+		for(int i = 0; i < matches.groupCount(); i += 2)
+			ret.add(new Point(Integer.parseInt(matches.group(i)), Integer.parseInt(matches.group(i+1))));
+		return ret;
+	}
 
 	public static List<String> readFile(String path) throws FileNotFoundException, IOException {
 		String line;
