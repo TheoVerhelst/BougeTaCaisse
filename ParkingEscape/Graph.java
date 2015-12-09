d7e687a52ea905006eb9bde4f09186a106e12d83
TheoVerhelst <tverhels@ulb.ac.be>
2015-12-10 00:20:48 +0100

    Implements Graph.addSituation

--- ParkingEscape/Graph.java
+++ ParkingEscape/Graph.java
@@ -1,6 +1,34 @@
 package ParkingEscape;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Graph {
+	private HashMap<Situation, Integer> situations;
+	private ArrayList<ArrayList<Boolean>> adjacencyMatrix;
+
 	public Graph(Situation initialSituation) {
+		addSituation(initialSituation);
+	}
+
+	private int addSituation(Situation situation) {
+		final int index = situations.size();
+		situations.put(situation, index);
+		final int matrixIndex = addMatrixEntry();
+		assert matrixIndex == index : "adjacencyMatrix and situations has different sizes.";
+		return index;
+	}
+
+	private int addMatrixEntry() {
+		final int index = adjacencyMatrix.size();
+		for(int i = 0; i < index; ++i)
+			adjacencyMatrix.get(i).add(false);
+		ArrayList<Boolean> newRow = new ArrayList<Boolean>();
+		newRow.ensureCapacity(index + 1);
+		for(int i = 0; i < index; ++i)
+			newRow.add(false);
+		newRow.add(true);
+		adjacencyMatrix.add(newRow);
+		return index;
 	}
 }
